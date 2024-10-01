2cba5f75abc924f243b6e79cbeeed599c0733dfa
Jefferson Vargas <jeffersonvargas745@gmail.com>
2024-10-01 17:11:36 +0000

    feat: Controller and Service changes

--- src/main/java/application/controller/CategoriaController.java
+++ src/main/java/application/controller/CategoriaController.java
@@ -1,28 +1,29 @@
 package application.controller;
-
+ 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+ 
 import application.model.Categoria;
-import application.repository.CategoriaRepository;
-
+import application.service.CategoriaService;
+ 
 @RestController
 @RequestMapping("/categorias")
 public class CategoriaController {
+ 
     @Autowired
-    CategoriaRepository categoriaRepo;
-
+    CategoriaService categoriaService;
+ 
     @GetMapping
     public Iterable<Categoria> findAll() {
-        return categoriaRepo.findAll();
+        return categoriaService.findAll();
     }
-
+ 
     @PostMapping
     public Categoria insert(@RequestBody Categoria categoria) {
-        return categoriaRepo.save(categoria);
+        return categoriaService.insert(categoria);
     }
 }
--- src/main/java/application/service/CategoriaService.java
+++ src/main/java/application/service/CategoriaService.java
@@ -1,8 +1,23 @@
 package application.service;
-
+ 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+ 
+import application.model.Categoria;
+import application.repository.CategoriaRepository;
+ 
 @Service
 public class CategoriaService {
-    
+ 
+    @Autowired
+    private CategoriaRepository caterogiaRepo;
+ 
+    public Iterable<Categoria> findAll() {
+        return caterogiaRepo.findAll();
+    }
+ 
+    public Categoria insert(Categoria categoria) {
+        return caterogiaRepo.save(categoria);
+    }
+ 
 }
