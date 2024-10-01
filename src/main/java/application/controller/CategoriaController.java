1178db408b1e968f51e7995d6109535a8c650e19
Jefferson Vargas <jeffersonvargas745@gmail.com>
2024-10-01 19:36:25 +0000

    feat: CategoriaDTO and Others

--- src/main/java/application/controller/CategoriaController.java
+++ src/main/java/application/controller/CategoriaController.java
@@ -1,13 +1,20 @@
 package application.controller;
  
+import java.util.NoSuchElementException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
- 
-import application.model.Categoria;
+import org.springframework.web.server.ResponseStatusException;
+
+import application.record.CategoriaDTO;
 import application.service.CategoriaService;
  
 @RestController
@@ -18,12 +25,27 @@ public class CategoriaController {
     CategoriaService categoriaService;
  
     @GetMapping
-    public Iterable<Categoria> findAll() {
+    public Iterable<CategoriaDTO> findAll() {
         return categoriaService.findAll();
     }
- 
+
+    @GetMapping("/{id}")
+    public CategoriaDTO findOne(@PathVariable long id) {
+        return categoriaService.findById(id);
+    }
+
     @PostMapping
-    public Categoria insert(@RequestBody Categoria categoria) {
+    public CategoriaDTO insert(@RequestBody CategoriaDTO categoria) {
         return categoriaService.insert(categoria);
     }
+
+    @DeleteMapping("/{id}")
+    public void deleteOne(@PathVariable long id) {
+        categoriaService.deleteById(id);
+    }
+
+    @PutMapping("/{id}")
+    public CategoriaDTO updateOne(@PathVariable long id, @RequestBody CategoriaDTO categoria) {
+            return categoriaService.update(id, categoria);
+        }
 }
--- src/main/java/application/service/CategoriaService.java
+++ src/main/java/application/service/CategoriaService.java
@@ -1,23 +1,63 @@
 package application.service;
  
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
- 
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.server.ResponseStatusException;
+
 import application.model.Categoria;
+import application.record.CategoriaDTO;
 import application.repository.CategoriaRepository;
  
 @Service
 public class CategoriaService {
  
     @Autowired
-    private CategoriaRepository caterogiaRepo;
+    private CategoriaRepository categoriaRepo;
  
-    public Iterable<Categoria> findAll() {
-        return caterogiaRepo.findAll();
+    public Iterable<CategoriaDTO> findAll() {
+        return categoriaRepo.findAll().stream().map(CategoriaDTO::new).toList();
     }
  
-    public Categoria insert(Categoria categoria) {
-        return caterogiaRepo.save(categoria);
+    public CategoriaDTO findById(long id) {
+        Optional<Categoria> resultado = categoriaRepo.findById(id);
+        if(resultado.isEmpty()) {
+            throw new ResponseStatusException(
+                HttpStatus.NOT_FOUND, "Categoria não encontrada"
+            );
+        }
+        return new CategoriaDTO(resultado.get());
+    }
+
+    public CategoriaDTO insert(CategoriaDTO categoria) {
+        Categoria nova = categoriaRepo.save(new Categoria(categoria));
+        return new CategoriaDTO(nova);
+    }
+
+    public void deleteById(long id) {
+        
+        if(!categoriaRepo.existsById(id)) {
+            throw new ResponseStatusException(
+                HttpStatus.NOT_FOUND, "Categoria não encontrada"
+            );
+        }
+        categoriaRepo.deleteById(id);
+    }
+
+    public CategoriaDTO update(long id, CategoriaDTO categoria) {
+        
+        if(!categoriaRepo.existsById(id)) {
+            throw new ResponseStatusException(
+                HttpStatus.NOT_FOUND, "Categoria não encontrada"
+            );
+        }
+        Categoria atualizado = categoriaRepo.save(new Categoria(categoria));
+        return new CategoriaDTO(atualizado);
     }
  
 }
